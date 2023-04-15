16e4c179147fc599c2b0528786e6caf82c4c075c
jmuzina <jmuzina@jmuzina.io>
2023-04-15 16:45:22 -0400

    reenable the splash for dev

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,13 @@ const routes: Routes = [
   {
     path: '',
     pathMatch:'full',
-    redirectTo: 'coming-soon',
-    //loadChildren: () => import('./modules/splash/splash.module').then(m => m.SplashModule),
+    //redirectTo: 'coming-soon',
+    loadChildren: () => import('./modules/splash/splash.module').then(m => m.SplashModule),
   },
-  {
+  /*   {
     path: 'coming-soon',
     loadChildren: () => import('./modules/staging/staging.module').then(m => m.StagingModule),
-  },
+  }, */
   {
     path: '**',
     component:PageNotFoundComponent,
