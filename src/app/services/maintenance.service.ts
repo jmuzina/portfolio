5c7b68f60540d594b8a722683aab9dc910e76cfb
Julie Muzina <julie.muzina@canonical.com>
2026-02-02 13:57:54 -0500

    maintenance.

--- src/app/services/maintenance.service.ts
+++ src/app/services/maintenance.service.ts
@@ -4,7 +4,12 @@ import { GenericService } from './generic.service';
 
 @Injectable({ providedIn: 'root' })
 export class MaintenanceService extends GenericService {
-  activeMaintenanceEvent?: MaintenanceEvent = undefined;
+  activeMaintenanceEvent?: MaintenanceEvent = {
+    id: 0,
+    started_at: new Date(2026, 2, 1, 0, 0, 0),
+    // ended_at: new Date(2026, 2, 1, 6, 0, 0),
+    message: 'Something better is coming...',
+  };
 
   constructor() {
     super();
